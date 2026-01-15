2d5f84c79951d99359b891618c7650b045e5e74e
stefangospic <stefan.gospic.1@gmail.com>
2026-01-15 11:54:20 +0100

    Definisati osnovna svojstva User klase

--- GozbaNaKlikApplication/Models/User.cs
+++ GozbaNaKlikApplication/Models/User.cs
@@ -3,9 +3,8 @@ namespace GozbaNaKlikApplication.Models;
 public class User
 {
     public int Id { get; set; }
-    public required string FullName { get; set; }
-    public required string Password { get; set; }
-    public string Role { get; set; } = "Kupac";
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
     
     //TODO: Dodati ostala svojstva korisnika
 }
