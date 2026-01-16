14e0f7359e4c090e9912758412a6b68069d23a20
stefangospic <stefan.gospic.1@gmail.com>
2026-01-16 09:49:44 +0100

    feat: Implementirana prijava korisnika.

--- GozbaNaKlikApplication/Controllers/UsersController.cs
+++ GozbaNaKlikApplication/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
+using System.Security.Claims;
 using GozbaNaKlikApplication.Data;
+using GozbaNaKlikApplication.DTOs.Auth;
+using GozbaNaKlikApplication.Models;
 using GozbaNaKlikApplication.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GozbaNaKlikApplication.Controllers;
@@ -15,5 +19,47 @@ public class UsersController:ControllerBase
         _userService = new UserService(context);
     }
 
-    //TODO: Definisati API endpoint-e (npr. [HttpGet], [HttpPost], [HttpPut], [HttpDelete])
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+    {
+        if (!request.IsValid())
+        {
+            return BadRequest("Username or password is incorrect");
+        }
+        try
+        {
+            User user = await _userService.Login(request.Username, request.Password);
+            
+            if (user == null)
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+            
+            var token = _userService.GenerateJwtToken(user);
+            return Ok(new
+            {
+                user.Id,
+                user.Username,
+                role = user.Role,
+                token
+            });
+        }
+        catch(Exception e)
+        {
+            return Unauthorized("Username or password is incorrect" + e.Message);
+        } 
+    }
+    
+
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        return Ok(new
+        {
+            User.Identity.Name,
+            Role = User.FindFirst(ClaimTypes.Role)?.Value
+        });
+    }
+
 }
--- GozbaNaKlikApplication/Services/UserService.cs
+++ GozbaNaKlikApplication/Services/UserService.cs
@@ -1,5 +1,10 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using GozbaNaKlikApplication.Data;
+using GozbaNaKlikApplication.Models;
 using GozbaNaKlikApplication.Repositories;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GozbaNaKlikApplication.Services;
 
@@ -12,5 +17,49 @@ public class UserService
         _userRepository = new UserRepository(context);
     }
     
-    //TODO: Dodati servise za korisnike (npr. GetAll, GetOne, Create, Update, Delete)
+    
+    public async Task<User> Login(string username, string password)
+    {
+        User user = await _userRepository.GetByUsername(username);
+        
+        if (user == null)
+        {
+            throw new Exception("Invalid username or password");
+        }
+
+        bool validPassword = BCrypt.Net.BCrypt.Verify(password, user.PasswordHash);
+
+        if (!validPassword)
+        {
+            throw new Exception("Invalid username or password");
+        }
+
+        return user;
+    }
+    
+    public string GenerateJwtToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("THIS_IS_A_VERY_SECRET_KEY_1234567890_ABCD")
+        );
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: "gozbanaklik",
+            audience: "gozbanaklik",
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
 }
