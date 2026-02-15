b08aab94c2bada588bfe54d8e8006b2a7dcabf86
Tijana <tjnmlsvljvc@gmail.com>
2026-02-15 17:50:02 +0100

    Dodavanje jela od strane vlasnika: DTO, interfejsi, repo, servis, kontroler

---
+++ GozbaNaKlikApplication/Controllers/MealController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using GozbaNaKlikApplication.Services.Interfaces;
+using GozbaNaKlikApplication.DTOs.Meal;
+using GozbaNaKlikApplication.Models;
+
+namespace GozbaNaKlikApplication.Controllers;
+
+[Route("api/restaurants/{restaurantId}/meals")]
+[ApiController]
+public class MealsController : ControllerBase
+{
+    private readonly IMealService _mealService;
+
+    public MealsController(IMealService mealService)
+    {
+        _mealService = mealService;
+    }
+
+    [Authorize(Roles = "Owner")]
+    [HttpPost]
+    public async Task<IActionResult> CreateMeal(int restaurantId, [FromBody] CreateMealDto dto)
+    {
+        try
+        {
+            Meal meal = await _mealService.CreateMealAsync(restaurantId, dto);
+            return Ok(new
+            {
+                meal.Id,
+                meal.Name,
+                meal.Description,
+                meal.Price,
+                meal.RestaurantId
+            });
+        }
+        catch (ArgumentException e)
+        {
+            return BadRequest(e.Message);
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+}
--- GozbaNaKlikApplication/DTOs/Restaurant/ShowRestaurantDto.cs
+++ GozbaNaKlikApplication/DTOs/Restaurant/ShowRestaurantDto.cs
@@ -7,6 +7,6 @@ namespace GozbaNaKlikApplication.DTOs.Restaurant
         public string Name { get; set; }
         public string? Description { get; set; }
         public string OwnerFullName { get; set; }
-        public List<Meal> Meals { get; set; } = new List<Meal>();
+        public List<GozbaNaKlikApplication.Models.Meal> Meals { get; set; } = new List<GozbaNaKlikApplication.Models.Meal>();
     }
 }
