fb78f0c7a161731d019b219adb46b7c37a65d1ed
Danilo <danilokuzman1@yahoo.com>
2026-03-26 21:02:38 +0100

    Dodat id u dto

--- GozbaNaKlikApplication/DTOs/Meals/ShowMealsDto.cs
+++ GozbaNaKlikApplication/DTOs/Meals/ShowMealsDto.cs
@@ -2,6 +2,7 @@
 {
     public class ShowMealsDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
         public decimal Price {  get; set; }
